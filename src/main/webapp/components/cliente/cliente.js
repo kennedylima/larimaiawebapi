b1aa5d13a8caa80dffecb54c620ad256683ac489
kennedylima <kennedyoliveiralima@gmail.com>
2015-12-08 14:06:21 -0300

    Prova 2 - Angular JS

--- src/main/webapp/components/cliente/cliente.js
+++ src/main/webapp/components/cliente/cliente.js
@@ -7,7 +7,7 @@ angular.module('app.cliente',['ngNewRouter'])
 
     $scope.clientes = {};//Lista de clientes que estão vinculados a tela
     $scope.cliente = {};//Objeto de cliente vinculado a tela
-    $scope.estado={};  
+    $scope.estado={};
     $scope.idEstadoSelecionado = 0;
     $scope.estados = [];
 
@@ -33,6 +33,7 @@ angular.module('app.cliente',['ngNewRouter'])
         $scope.listar();
        });
 
+        
     };
 
 
